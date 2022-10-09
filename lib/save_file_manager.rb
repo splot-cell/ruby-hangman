4e0d7e9a1ea0ff0e81fa604fbe2c588d8642d27e
Olly Seber <olly_s@hotmail.co.uk>
2022-10-09 09:46:45 -0700

    Implement saving and loading multiple files

--- lib/save_file_manager.rb
+++ lib/save_file_manager.rb
@@ -19,11 +19,52 @@ class SaveFileManager
     @saves_dir
   end
 
+  def filename_adjectives
+    %w[sleepy jumpy feisty humble proud]
+  end
+
+  def filename_nouns
+    %w[panda octopus lizard kangeroo lemur]
+  end
+
   def generate_filename
-  "#{self.class.saves_dir}uniqfilname.yaml"
+  "#{self.class.saves_dir}" \
+    "#{filename_adjectives.sample}_#{filename_nouns.sample}.yaml"
   end
 
   def load
-    YAML.load(File.read(generate_filename))
+    return no_saves if file_list.empty?
+
+    YAML.load(File.read("#{self.class.saves_dir}#{file_list[select_file]}"))
+  end
+
+  def no_saves
+    puts no_save_files_msg
+    {}
+  end
+
+  def select_file
+    puts file_list_msg
+    selection = gets.chomp
+    return selection.to_i if selection.match(/^[0-9]$/) &&
+           selection.to_i < file_list.length
+    puts "Selection not recognized\n\n"
+    select_file
+  end
+
+  def file_list
+    Dir.children(self.class.saves_dir)
+  end
+
+  def no_save_files_msg
+    "No savefiles detected, starting new game..."
+  end
+
+  def file_list_msg
+    msg = ["Enter a number to select a file from the filelist:\n"]
+    file_list.each_with_index do |file, index|
+      msg.push("[#{index}]\t#{file}")
+    end
+    msg.join("\n")
   end
 end
