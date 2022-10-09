e3dd8f3c94d0af7dcfe7a0cba88978e253753bba
Olly Seber <olly_s@hotmail.co.uk>
2022-10-08 17:39:37 -0700

    Add game logic and endstates

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -10,13 +10,15 @@ class Hangman
   end
 
   def run
-    play_turn
+    puts play_turn
   end
 
   def play_turn
+    return out_of_guesses_msg if incorrect_guesses_left.zero?
     puts remaining_guesses_msg
     puts current_guess_msg
-    guessed_letters.push(player_guess)
+    evaluate_guess(player_guess)
+    return solved_msg if solved?
     play_turn
   end
 
@@ -37,15 +39,32 @@ class Hangman
     progress.join
   end
 
+  def evaluate_guess(letter)
+    @incorrect_guesses_left -= 1 unless target_word.include?(letter)
+    guessed_letters.push(letter)
+  end
+
   def player_guess
     print "Guess a letter: "
     guess = gets.chomp[0]
-    if guess.match(/[a-z]/i)
+    if guess.to_s.match(/[a-z]/i)
       return guess unless guessed_letters.include?(guess)
       puts "You have already guessed that letter!"
     else
       puts "Please ensure the first character is an identifiable letter!"
     end
     player_guess
+  end
+
+  def solved?
+    guess_progress == target_word
+  end
+
+  def out_of_guesses_msg
+    "You ran out of guesses! The correct answer was #{target_word}."
+  end
+
+  def solved_msg
+    "#{target_word} is correct! Well done!"
   end
 end
